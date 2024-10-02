3ac28737bed1ce52f01f10beca8cd4f85cc1fd83
Karl Brycz <kbrycz@umich.edu>
2024-10-01 21:30:37 -0500

    Initial Commit

---
+++ justpark/justparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  justparkApp.swift
+//  justpark
+//
+//  Created by Karl Brycz on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct justparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
