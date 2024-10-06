9bd2eb8b2dab0eb5d4a754a9333a6bacf0df5b13
Karl Brycz <kbrycz@umich.edu>
2024-10-06 13:03:38 -0500

    Reverting back to normal working without location / pin

--- justpark/ViewModels/LocationManager.swift
+++ justpark/ViewModels/LocationManager.swift
@@ -2,28 +2,14 @@
 
 import Foundation
 import MapKit
-import CoreLocation
 
-class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    @Published var region: MKCoordinateRegion
+class LocationManager: NSObject, ObservableObject {
+    @Published var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 41.9500, longitude: -87.6590), // Centered on Lakeview
+        span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+    )
 
     @Published var selectedRoad: Road?
-
-    private let locationManager = CLLocationManager()
-
-    override init() {
-        // Set the hardcoded user location
-        let hardcodedLocation = CLLocationCoordinate2D(latitude: 41.9500, longitude: -87.6590)
-        self.region = MKCoordinateRegion(
-            center: hardcodedLocation,
-            span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-        )
-        super.init()
-        locationManager.delegate = self
-
-        // Simulate location update with hardcoded location
-        self.locationManager(locationManager, didUpdateLocations: [CLLocation(latitude: hardcodedLocation.latitude, longitude: hardcodedLocation.longitude)])
-    }
 
     func zoomIn() {
         region.span.latitudeDelta /= 2
@@ -33,19 +19,5 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     func zoomOut() {
         region.span.latitudeDelta *= 2
         region.span.longitudeDelta *= 2
-    }
-
-    // CLLocationManagerDelegate methods
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // Use hardcoded user location
-        if let location = locations.last {
-            DispatchQueue.main.async {
-                self.region.center = location.coordinate
-            }
-        }
-    }
-
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Failed to get user location: \(error)")
     }
 }
--- justpark/Views/MapView.swift
+++ justpark/Views/MapView.swift
@@ -12,19 +12,14 @@ struct MapView: UIViewRepresentable {
         let mapView = MKMapView(frame: .zero)
         mapView.delegate = context.coordinator
 
-        // Show user's location
-        mapView.showsUserLocation = true
+        // Do not show the user's location
+        mapView.showsUserLocation = false
 
         // Set the map's initial region
         mapView.setRegion(locationManager.region, animated: true)
 
         // Exclude all points of interest
         mapView.pointOfInterestFilter = MKPointOfInterestFilter.excludingAll
-
-        // Add long-press gesture recognizer
-        let longPressRecognizer = UILongPressGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handleLongPress(_:)))
-        longPressRecognizer.minimumPressDuration = 0.5
-        mapView.addGestureRecognizer(longPressRecognizer)
 
         return mapView
     }
@@ -45,8 +40,76 @@ struct MapView: UIViewRepresentable {
     }
 
     func makeCoordinator() -> Coordinator {
-        Coordinator(self, locationManager: locationManager)
+        Coordinator(self)
     }
 
-    typealias Coordinator = MapViewCoordinator
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+
+        // Renderer for overlays
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            if let roadOverlay = overlay as? RoadOverlay {
+                let renderer = MKPolylineRenderer(polyline: roadOverlay)
+
+                if let road = roadOverlay.road {
+                    let status = road.status()
+
+                    switch status {
+                    case "red":
+                        renderer.strokeColor = UIColor.red.withAlphaComponent(0.7)
+                    case "yellow":
+                        renderer.strokeColor = UIColor.yellow.withAlphaComponent(0.7)
+                    case "clear":
+                        renderer.strokeColor = UIColor.green.withAlphaComponent(0.1) // Very transparent
+                    default:
+                        renderer.strokeColor = UIColor.gray.withAlphaComponent(0.7)
+                    }
+                } else {
+                    renderer.strokeColor = UIColor.gray.withAlphaComponent(0.7)
+                }
+
+                renderer.lineWidth = 4
+                return renderer
+            }
+            return MKOverlayRenderer()
+        }
+
+        // Handle annotation views
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            guard annotation is RoadAnnotation else { return nil }
+
+            let identifier = "RoadAnnotation"
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+            if annotationView == nil {
+                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = false
+            } else {
+                annotationView?.annotation = annotation
+            }
+
+            // Make the annotation view invisible but with a larger touch area
+            annotationView?.isEnabled = true
+            annotationView?.alpha = 0.001
+            annotationView?.frame.size = CGSize(width: 44, height: 44) // Increase touch area
+            annotationView?.centerOffset = CGPoint(x: 0, y: 0)
+
+            return annotationView
+        }
+
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+            if let roadAnnotation = view.annotation as? RoadAnnotation,
+               let road = roadAnnotation.road {
+                DispatchQueue.main.async {
+                    self.parent.locationManager.selectedRoad = road
+                }
+            }
+            // Deselect the annotation to allow re-selection
+            mapView.deselectAnnotation(view.annotation, animated: false)
+        }
+    }
 }
